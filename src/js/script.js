f08b599b4f2b1302534474bb119c6596c11e3479
olive027 <blueblueww@yahoo.co.jp>
2023-10-15 22:29:50 +0900

    1015

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,10 @@
 
 jQuery(function ($) { // この中であればWordpressでも「$」が使用可能になる
-
+// hamburger
+$('.js-hamburger').click(function(){
+	$('.js-hamburger').toggleClass('is-active');
+	$('.js-sp-nav').fadeToggle();
+	// $('.js-sp-nav').toggleClass('is-active');
+})
 
 });
