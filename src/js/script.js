bc2575c4e7d3f2d8bd8da4c42064e15bf82feaef
olive027 <blueblueww@yahoo.co.jp>
2023-10-16 22:59:07 +0900

    1016

--- src/js/script.js
+++ src/js/script.js
@@ -3,8 +3,24 @@ jQuery(function ($) { // この中であればWordpressでも「$」が使用可
 // hamburger
 $('.js-hamburger').click(function(){
 	$('.js-hamburger').toggleClass('is-active');
-	$('.js-sp-nav').fadeToggle();
+	$('.js-sp-nav').fadeToggle(600);
+	$('html').toggleClass('is-fixed');
 	// $('.js-sp-nav').toggleClass('is-active');
 })
+
+// swiper
+const swiper = new Swiper(".swiper", {
+	loop: true,
+	autoplay: {
+		delay: 3000,
+		disableOnInteraction: false,
+	},
+	pagination: {
+		el: '.swiper-pagination',
+	},
+	effect: 'fade',
+	speed: 3000,
+	allowTouchMove: false,
+});
 
 });
